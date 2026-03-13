3115fd7cae452680d20d689c6c0934e007237d8c
0xp3p3x0 <lucafarrugia700@gmail.com>
2026-03-12 22:23:18 -0700

    fix issues

--- internal/server/router.go
+++ internal/server/router.go
@@ -29,7 +29,7 @@ func SetupRouter(
 
 	// Add CORS middleware
 	router.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:3000", "https://dev-exchange.she.io", "https://www.she.io", "https://she.io"},
+		AllowOrigins:     []string{"http://localhost:8080", "https://www.fantasygaming.games", "https://fantasygaming.games"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization", "X-Requested-With"},
 		ExposeHeaders:    []string{"Content-Length"},
